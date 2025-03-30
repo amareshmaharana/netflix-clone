4090e12b50d4dd7f7559c5ae3acf858bae0f6151
Amaresh Maharana <amareshcoder@gmail.com>
2025-03-30 13:50:50 +0530

    initialize project structure and succefully created Home page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <>
+        <div className='login'>
+            <h1>Login form</h1>
+        </div>
+    </>
+  )
+}
+
+export default Login
